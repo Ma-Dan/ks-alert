59e55b3da7c4615a9cba2a7fa2392abe8ddb9038
Carman Zhang <carmanzhang@yunify.com>
2018-10-23 11:01:22 +0800

    first run sqls

--- document/ks_alert_sql.sql
+++ document/ks_alert_sql.sql
@@ -2,7 +2,7 @@
 create table resource_level (
 	resource_level_id varchar(50) not null primary key,
 	resource_type varchar(50) not null,  -- "cluster" "node" "workspace"
-	description text default '' not null,
+	description text,
 	enable boolean default true,
 	create_by varchar(50),
 	create_time timestamp default current_timestamp not null,
@@ -34,7 +34,7 @@ create table resource_group (
 create table metric (
   metric_id varchar(50) not null primary key,
   metric_name varchar(100) not null,
-  rule_promql text not null,
+  rule_promql text,
   resource_level_id varchar(50) not null,
   resource_type varchar(50) not null,
   update_time timestamp default current_timestamp not null
@@ -43,7 +43,7 @@ create table metric (
 -- alert rule，
 create table alert_rule (
 	alert_rule_id varchar(50) not null primary key,
-	alert_name text default '' not null,
+	alert_name text,
 	alert_group_id varchar(50) not null, -- 所属的告警规则组
   -- 	description text default '' not null,
 	metric_id varchar(50) not null,
@@ -79,7 +79,7 @@ create table alert_rule_group (
 	enable boolean default true not null, -- 该告警组是否启动，该告警组可能属于系统或用户
 	create_time timestamp default current_timestamp not null,
 	update_time timestamp default current_timestamp not null,
-	create_by varchar(255) not null,  -- 系统创建的规则还是用户创建的规则模板
+	create_by varchar(255) not null -- 系统创建的规则还是用户创建的规则模板
 );
 
 -- 告警的严重程度
@@ -135,14 +135,14 @@ create table alert_binding (
   silence_enable boolean default false,
   silence_start timestamp default current_timestamp not null,
   silence_end timestamp default current_timestamp not null,
-  repeat_type varchar(20) default exponential,  -- 多久重复一次告警通知，重发周期保持不变 stationary 还是服从指数 exponential
+  repeat_type varchar(20) default 'exponential',  -- 多久重复一次告警通知，重发周期保持不变 stationary 还是服从指数 exponential
   repeat_interval int default 8,  -- 单位是小时, 如果重发周期是固定的 stationary，该字段才有效
   init_repeat_period int default 1,  --  如果重发周期是exponential， 初始重发周期是 1小时
   max_repeat_period int default 168,  --  如果重发周期是exponential， 最大重发周期是 168 小时。
   enable_time timestamp default current_timestamp not null,  -- 告警的开始时间， 若不设置，则就是当前时间
 	disable_time timestamp default current_timestamp not null,  -- 告警的结束时间， 若不设置，则告警一直有效
 	enable boolean default true not null, -- 该告警组是否启用
-	description text default '' not null  -- 告警描述
+	description text -- 告警描述
 );
 
 -- alert history 只记录和alert状态变更相关的 记录，用来推算alert的开始时间-结束时间
@@ -156,8 +156,8 @@ create table alert_history (
 	resource_type varchar(50) not null, -- "node" , "pod"  ...
 	resource_group_id varchar(50) not null, -- 用户自定义的资源组
 	resource_group_name varchar(50) not null,
-	resource_names text not null, -- 该时刻所有的资源,以数组的形式展示
-	alerted_resource text not null, -- 正在触发告警的资源
+	resource_names text, -- 该时刻所有的资源,以数组的形式展示
+	alerted_resource text, -- 正在触发告警的资源
 
 	severity varchar(20) not null, -- 严重程度 与 receiver_group 绑定
 	receiver_group_name varchar(50) not null,
@@ -168,5 +168,5 @@ create table alert_history (
 	trigger_alert_rule varchar(200) not null, -- 触发告警的 alert rule
 	event_time timestamp default current_timestamp not null, -- alert变更的时间，
 	operation varchar(50) not null, -- alert被触发， alert被silenced， alert 起作用时间已到， alert 被删除。
-  description text default '' not null
+  description text
 );
